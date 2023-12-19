a4d3b96c47b693ba9fa2c260bf645d04fa06f9cc
goku-raj <anshuraj1479@gmail.com>
2023-12-19 23:37:15 +0530

    Listing4-(Image Upload)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -12,6 +12,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
     CATEGORY = 0,
@@ -53,6 +54,7 @@ const RentModal = () => {
     const guestCount = watch('guestCount');
     const roomCount = watch('roomCount');
     const bathroomCount = watch('bathroomCount');
+    const imageSrc = watch('imageSrc')
 
     const Map = useMemo(() => dynamic(() => import('../Map'), {
         ssr: false
@@ -153,6 +155,21 @@ const RentModal = () => {
                     onChange={(value) => setCustomValue('bathroomCount', value)}
                 />
                 <hr />
+            </div>
+        )
+    }
+
+    if (step === STEPS.IMAGES) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Add a photo of your place"
+                    subtitle="show you guests what your place looks like!"
+                />
+                <ImageUpload
+                    value={imageSrc}
+                    onChange={(value) => setCustomValue('imageSrc', value)}
+                />
             </div>
         )
     }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const { hostname } = require("os");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+    domains: [
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com",
+    ],
   },
 };
 
